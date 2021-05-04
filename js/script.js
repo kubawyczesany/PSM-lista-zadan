d837e4073751271db3bb26cbcbf80fc8b9094500
Olga Sowinska <olga.sowinska00@gmail.com>
2021-05-04 14:42:26 +0200

    now?

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("serviceWorker.js", { scope: '/' })
+      .register("serviceWorker.js", { scope: 'https://olgasowlet.github.io/PSM-lista-zadan/' })
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
