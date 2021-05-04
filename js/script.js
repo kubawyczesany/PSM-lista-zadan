35c001f5fa78ae045742346d8b8ba897c3394d00
Olga Sowinska <olga.sowinska00@gmail.com>
2021-05-04 14:19:42 +0200

    fixed manifest

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
+  window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("../serviceWorker.js")
       .then(res => console.log("service worker registered"))
