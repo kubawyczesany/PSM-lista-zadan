17a5aea3751833ca82b1a9a88c62ff4e30b3a6bf
Olga Sowinska <olga.sowinska00@gmail.com>
2021-05-04 17:07:49 +0200

    changed filesToCache

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,19 +2,19 @@ var cacheName = 'zadania';
 var filesToCache = [
   'https://olgasowlet.github.io/PSM-lista-zadan/',
   'https://olgasowlet.github.io/PSM-lista-zadan/index.html',
-  '/',
-  '/index.html',
-  '/css/button.css',
-  '/css/container.css',
-  '/css/form.css',
-  '/css/header.css',
-  '/css/style.css',
-  '/css/tasks.css',
-  '/images/icon.svg',
-  '/images/task.png',
-  '/js/script.js',
-  '/js/authentication.js',
-  '/js/tasks.js'
+  './',
+  './index.html',
+  './css/button.css',
+  './css/container.css',
+  './css/form.css',
+  './css/header.css',
+  './css/style.css',
+  './css/tasks.css',
+  './images/icon.svg',
+  './images/task.png',
+  './js/script.js',
+  './js/authentication.js',
+  './js/tasks.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
@@ -22,7 +22,7 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
-    })
+    }).catch(err => console.log(err))
   );
 });
 
