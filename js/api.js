69af4fd1863421093fdc2b8a56ddb3eaaf98890c
Olga Sowinska <olga.sowinska00@gmail.com>
2021-05-06 15:18:52 +0200

    added geolocation

--- js/api.js
+++ js/api.js
@@ -1,3 +1,5 @@
+// Wibracje
+
 let addButtonElement = document.querySelector(".js-button-addTask");
 
 addButtonElement.addEventListener("click", () => {
@@ -12,3 +14,35 @@ function vibratePattern () {
   navigator.vibrate([400, 100, 400, 100, 400]);
 }
                     
+// Geolokacja
+function geoFindMe() {
+
+  const status = document.querySelector('#status');
+  const mapLink = document.querySelector('#map-link');
+
+  mapLink.href = '';
+  mapLink.textContent = '';
+
+  function success(position) {
+    const latitude  = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    status.textContent = '';
+    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+    mapLink.innerHTML = `Szerokość: ${latitude} ° </br> Wysokość: ${longitude} °`;
+  }
+
+  function error() {
+    status.textContent = 'Nie znaleziono lokalizacji';
+  }
+
+  if(!navigator.geolocation) {
+    status.textContent = 'Geolokacja nie jest wspierana przez tę przeglądarkę';
+  } else {
+    status.textContent = 'Lokalizowanie...';
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
+
+}
+
+document.querySelector('#find-me').addEventListener('click', geoFindMe);
